cc6a8606f6e0f91f741d9532dd4935cb922d34d7
bergsm <bergsha@os1.engr.oregonstate.edu>
2018-06-11 14:52:40 -0700

    11062018 16:52

--- compileall.sh
+++ compileall.sh
@@ -3,4 +3,7 @@
 gcc -g -std=gnu99 keygen.c -o keygen
 gcc -g -std=gnu99 otp_enc_d.c -o otp_enc_d
 gcc -g -std=gnu99 otp_enc.c -o otp_enc
+gcc -g -std=gnu99 otp_dec_d.c -o otp_dec_d
+gcc -g -std=gnu99 otp_dec.c -o otp_dec
+gcc -g -std=gnu99 otp.c -o otp
 
